5d6d4318aba9d1fe0f0dd11e801d512501376fe9
Matthew Freeman <matthew.freeman821@gmail.com>
2019-01-31 13:21:39 -0500

    Adds global and DOM element variables

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Global variables
+let time = 5;
+let score = 0;
+let isPlaying;
+
+//DOM element variables
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
